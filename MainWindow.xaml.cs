a07eb7c240f8926879b43ea73db7a39069a29d19
zkdlu <zkdlu159951@gmail.com>
2020-08-27 22:05:41 +0900

    preview box

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PreviewBoxTest
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Thumb_DragDelta(object sender, System.Windows.Controls.Primitives.DragDeltaEventArgs e)
+        {
+            Canvas.SetLeft(MouseThumb, Canvas.GetLeft(MouseThumb) + e.HorizontalChange);
+            Canvas.SetTop(MouseThumb, Canvas.GetTop(MouseThumb) + e.VerticalChange);
+        }
+    }
+}
